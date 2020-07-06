f09ee532a0f5713b2028abc444876b9cb3bb0724
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-06 15:22:01 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,19 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+
+def puts_phrase_into_code
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+
+puts_phrase_into_code
+
